836f9d2b1c0aed02cd6e426fd71309d969795c3c
croye <Colin.Roye@stonybrook.edu>
2019-03-19 17:48:37 -0400

    More progress on accessors and initializers for free list

--- src/sf_listHelpers.c
+++ src/sf_listHelpers.c
@@ -4,35 +4,62 @@
 #include "debug.h"
 #include "sfmm.h"
 
-void * splitBlock(void* ptr, size_t block_size){
+/*return lsb of header*/
+int getNextAlloc(void* ptr){
+    return (int)((long int*)(ptr*) & 1);
+}
+int getPrevAlloc(void* ptr){
+    return (int)((long int*)(ptr*) & 2) >> 1;
+}
+/*return last 32 lsb(s)*/
+size_t getBlockSize(void* ptr){
+    return (size_t)((long int*)ptr*) & 0xFFFFFFFF;
+}
+
+size_t getRequestedSize(void* ptr){
+    return (size_t)((long int*)ptr*) >> 32;
+}
+
+
+void setPrevAlloc(void* ptr, int prevAlloc){
+    return 1;
+}
+
+void setNextAlloc(void* ptr, int nextAlloc){
+    return 1;
+}
+
+void setBlockSize(void* ptr, size_t block_size){
+    return 1;
+}
+
+void setRequestedSize(void* ptr, size_t requested_size){
+    return 1;
+}
+
+
+void splitBlock(void* ptr, size_t block_size, size_t requested_size){
     int other_block_size = getBlockSize(ptr) - block_size;
     //check for splinters
     if(other_block_size < EFFECTIVE_BLOCK_SIZE){
         return NULL;
     }
+    //get variables for free block
+    void* prev = getPrevPtr(ptr);
+    void* next = getNextPtr(ptr);
+    int nextAlloc = getNextAlloc(ptr);
+
     //set allocated block
+    setBlockSize(ptr, requested_size);
     setBlockSize(ptr, block_size);
     setNextAlloc(ptr, 0);
     //set free block
     ptr = (char*) ptr + block_size;
-    setBlockSize
+    initFreeBlock(ptr, prev, next, requested_size, block_size, 1, nextAlloc)
 
 }
 
-int setPrevNextAlloc(void* ptr, int prevAlloc, int nextAlloc){
-    return 1;
-}
-
-int setBlockSize(void* ptr, size_t block_size){
-    return 1;
-}
-
-int setRequestedSize(void* ptr, size_t requested_size){
-    return 1;
-}
-
-
-int setFooter(void* ptr, size_t block_size, int prevAlloc, int nxtAlloc){
+void setFooter(void* ptr, size_t block_size, int prevAlloc, int nxtAlloc){
     ptr = (char*)ptr + (block_size -8);
     setRequestedSize(ptr, 0);
     setBlockSize(ptr, block_size);
@@ -41,26 +68,23 @@ int setFooter(void* ptr, size_t block_size, int prevAlloc, int nxtAlloc){
     return 1;
 }
 /*wrapper for setFooter, setFooter == setFreeHeader*/
-int setFreeHeader(void* ptr, size_t block_size, int prevAlloc, int nextAlloc, void* prev, void* next, size_t requested_size,){
+void setFreeHeader(void* ptr, size_t block_size, int prevAlloc, int nextAlloc, void* prev, void* next, size_t requested_size){
     setRequestedSize(ptr, 0);
     setBlockSize(ptr, block_size);
     setPrevAlloc(ptr, prevAlloc);
     setNextAlloc(ptr, nextAlloc);
     setNext(ptr, next);
     setPrev(ptr, prev);
-
-    return 1;
 }
-int initFreeBlock(void* ptr, void* prev, void* next, size_t requested_size, size_t block_size, int prevAlloc, int nextAlloc){
-    setFreeHeader()
+void initFreeBlock(ptr, prev, next, requested_size, block_size, prevAlloc, nextAlloc){
+    setFreeHeader(ptr, block_size, prevAlloc, nextAlloc, prev, next, size_t requested_size,)
+    setFooter(ptr, block_size, prevAlloc, nxtAlloc)
 }
 /*Allocated header is the same as set footer, with 2 additional rows (previous in list, next in list)*/
 /*NOTE: prev/next in list is not prev/next alloc, it is next in freeList */
-int setAllocHeader(void* ptr, size_t block_size, int prevAlloc, int nextAlloc, size_t requested_size){
+void setAllocHeader(void* ptr, size_t block_size, int prevAlloc, int nextAlloc, size_t requested_size){
     setRequestedSize(ptr, requested_size);
     setBlockSize(ptr, block_size);
     setPrevAlloc(ptr, prevAlloc);
     setNextAlloc(ptr, nextAlloc);
-
-    return 1;
 }
